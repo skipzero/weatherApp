ebb01928c63bf5f75c3ffe8aa0b0ac0c86eff8a8
zerosquadron <skip@zerosquadron.com>
2016-06-08 00:11:30 -0700

    added redis

--- weatherServer.js
+++ weatherServer.js
@@ -1,18 +1,25 @@
-var http 		= require('http')
-	, getPath 	= 'http://10.0.0.35';
+const http   		= require('http');
+const getPath 	= 'http://10.0.0.35';
+const redis     = require('redis');
+const client    = redis.createClient();
 
 http.get(getPath, function callback(resp) {
 	resp.setEncoding('utf8')
 
 	resp.on('data', function(data){
-		console.log(data)
+		console.log(data);
 		return weatherObject(data);
 	})
 	resp.on('error', function(error){
-		console.log(error)
+		console.log('ERR', error)
 	})
 })
 
 const weatherObject = data => {
 
 }
+
+
+client.on('connect', () => {
+  console.log('redis connection...')
+});
