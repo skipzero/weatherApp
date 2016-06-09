fba00978590b665f03b42b96b16d7af09631ae10
Skip McBride <skip@zerosquadron.com>
2016-06-09 09:14:29 -0700

    working on server and weather call

--- src/weatherServer.js
+++ src/weatherServer.js
@@ -1,18 +1,50 @@
-const http   		= require('http');
+'use strict';
+
 const getPath 	= 'http://10.0.0.35';
+const http   		= require('http');
+const port 			= 5050;
 
-http.get(getPath,  (resp) => {
-	resp.setEncoding('utf8')
+const server = http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write(`<!DOCTYPE html>
+									<html>
+									<head>
+									<title>Hello World Page</title>
+									</head>
+									<body>
+									Hello World!
+									</body>
+									</html>`);
+  response.end();
+});
 
-	resp.on('data', (data) => {
-		console.log(data);
-		return weatherObject(data);
-	})
-	resp.on('error', (error) => {
-		console.log('ERR', error)
-	})
-})
+const getWeather = (resp) => {
+	return http.get(getPath,  (resp) => {
+		resp.setEncoding('utf8');
+		let body = '';
 
-const weatherObject = data => {
+		resp.on('data', (data) => {
+			console.log('data', data);
+			body += data;
+		});
 
+		resp.on('error', (error) => {
+			console.log('ERR', error)
+		});
+
+		resp.on('end', () => {
+			let parsed = JSON.parse(body);
+
+			cb({
+				pdata: parsed
+			})
+		});
+	});
 }
+
+const weatherObject = () => {
+	let data = getWeather();
+}
+
+server.listen(port);
+console.log('Server is listening on port: ' + port);
