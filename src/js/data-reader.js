a35e1928aa052962dd6c36b7dc6bf827081eaccc
Horseman #1 <skip@zerosquadron.com>
2016-07-17 16:32:14 -0700

    cleaning up and reading db info

--- src/js/data-reader.js
+++ src/js/data-reader.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 
 const dataReader = () => {
   connection.query('SELECT * FROM `weather`.`data_table` Limit 15,5', (err, res, fields) => {
-    console.log(`Res:`, res);
-    console.log(`Fields:`, fields);
+    console.log(`Res: ${res}`);
+    console.log(`Fields: ${fields}`);
   });
 };
 
