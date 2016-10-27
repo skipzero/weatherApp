d8db84adf051d5d9d8a04a98d6f9f9bd9d441023
Skip McBride <skip@zerosquadron.com>
2016-10-26 22:41:17 -0700

    fixing py script

--- gpio.py
+++ gpio.py
@@ -1,6 +1,19 @@
+#!/usr/bin/python
+
+import sys
 import RPi.GPIO as GPIO
-GPIO.setup(4, OUT)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.OUT)
+
+arg = sys.arv[1]
+
+if arg == 'on':
+    cmd = True
+
+if arg == 'off':
+    cmd = False
 
 while true:
-    # GPIO.output(4, True)
-    print 'Working for it....'
+    # GPIO.output(23, True)
+    print 'Working for it....' + cmd
