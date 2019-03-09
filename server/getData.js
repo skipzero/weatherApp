cd8c66ed324c22e2b67f1c8f43fd04262709229b
bunny <bunny@angerbunny-v0.2>
2019-03-09 03:50:36 +0000

    shelfing for the night

--- server/getData.js
+++ server/getData.js
@@ -1,10 +1,10 @@
 /*eslint no-console: ['error', { allow: ['log', 'info', 'error'] }] */
 
 
-const convert = require('./converter');
+const converter = require('./converter');
 const https = require('https');
 const writeData = require('./writeData');
-const apiKey = process.env.API_KEY;
+const apiKey = process.env.API_KEY2;
 const location = '37.814264,-122.243132';
 // const extWeatherApi = `http://api.darksky.net/forecast/${apiKey}/${location}`;
 
@@ -13,29 +13,32 @@ const getData = () => {
 
   const ForecastIo = require('forecastio');
   const forecastIo = new ForecastIo(apiKey);
+console.log('APIkey', apiKey);
 
-  forecastIo.forecast('37.8', '-122').then(data => convert(data.currently)).then(data => writeData(data)).catch(error => console.error('Error:', error));
+  forecastIo.forecast('37.8', '-122').then(data => convert(data)).then(data => writeData(data)).catch(error => console.error('Error:', error));
 
-  const weatherAddress = `https://api.forecast.io/forecast/${process.env.API_KEY}/${location}`;
+  //const weatherAddress = `https://api.forecast.io/forecast/${process.env.API_KEY}/${location}`;
 
-  let rawData;
-  https.get(weatherAddress, (resp) => {
-    resp.setEncoding('utf8');
-    resp.on('data', (data) => {
-      rawData += data;
-    })
-    resp.on('end', () => {
+//  let rawData;
+//  https.get(weatherAddress, (resp) => {
+//    resp.setEncoding('utf8');
+//    resp.on('data', (data) => {
+//.console.log('Dattty', data);
+//      rawData += data;
+//    })
+//    resp.on('end', () => {
       // const newData = JSON.parse(rawData);
-      rawData = rawData.slice(9)
-      rawData = JSON.parse(rawData);
-      rawData = rawData.currently;
-      convert(rawData)
-    });
-  });
+ //     rawData = rawData.slice(9)
+ //     rawData = JSON.parse(rawData);
+//console.log('End', rawData);
+//      convert(rawData)
+//    });
+//  });
 
   function convert(data) {
-    const newData = JSON.parse(data);
-    console.log('Convert', newData['latitude'])[0]
+    console.log(JSON.stringify(data));
+//    const newData = JSON.parse(data);
+    console.log('Convert', data['latitude'])
   }
 };
 
