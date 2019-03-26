95d7789cb063fddaa6b89c0968737d7dd29f4ba1
skipzero <skipmcbride@dagger.local>
2019-03-25 20:30:55 -0700

    fixed wonky merge conflicts

--- webpack.config.js
+++ webpack.config.js
@@ -29,32 +29,24 @@ module.exports = {
       Tether: 'tether',
       'window.Tether': 'tether',
       Popper: ['popper.js', 'default'],
-      Tooltip: 'exports-loader?Tooltip!bootstrap/js/dist/tooltip',
+      // Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
+      // Button: 'exports-loader?Button!bootstrap/js/dist/button',
+      // Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
+      // Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
+      // Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
+      // Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
+      // Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
+      // Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
+      // Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
+      // Tooltip: 'exports-loader?Tooltip!bootstrap/js/dist/tooltip',
+      // Util: 'exports-loader?Util!bootstrap/js/dist/util',
     }),
   ],
   module: {
     rules: [
       {
-        test: /\.(scss)$/,
-        use: ExtractTextPlugin.extract({
-          loader: 'style-loader', // inject CSS to page
-        }, {
-            loader: 'css-loader', // translates CSS into CommonJS modules
-          }, {
-            loader: 'postcss-loader', // Run post css actions
-            options: {
-              plugins: function() { // post css plugins, can be exported to postcss.config.js
-                return [
-                  require('precss'),
-                  require('autoprefixer'),
-                  new DashboardPlugin({ port: 3001 }),
-                ];
-              }
-            }
-          }, {
-            loader: 'sass-loader' // compiles Sass to CSS
-          }
-        ),
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
       {
         test: /\.scss$/,
@@ -76,7 +68,4 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new ExtractTextPlugin("styles.css"),
-  ]
 };
